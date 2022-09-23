a6bfb8d906acbbda54ef4abab09f07ac83f61e42
khomuk <i.khomuk@gmail.com>
2022-09-23 16:55:47 +0200

    update

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,7 @@ const gulpif = require("gulp-if");
 const notify = require("gulp-notify");
 const image = require("gulp-imagemin");
 const { readFileSync } = require("fs");
+// const typograf = require("gulp-typograf");
 const webp = require("gulp-webp");
 const avif = require("gulp-avif");
 const mainSass = gulpSass(sass);
@@ -272,20 +273,23 @@ const avifImages = () => {
 };
 
 const htmlInclude = () => {
-  return src([`${srcFolder}/*.html`])
-    .pipe(
-      fileInclude({
-        prefix: "@",
-        basepath: "@file",
-      })
-    )
-    .pipe(
-      typograf({
-        locale: ["ru", "en-US"],
-      })
-    )
-    .pipe(dest(buildFolder))
-    .pipe(browserSync.stream());
+  return (
+    src([`${srcFolder}/*.html`])
+      .pipe(
+        fileInclude({
+          prefix: "@",
+          basepath: "@file",
+        })
+      )
+      //.pipe(
+      //  typograf({
+      //    locale: ["ru", "en-US"],
+      //  }
+      //  )
+      //)
+      .pipe(dest(buildFolder))
+      .pipe(browserSync.stream())
+  );
 };
 
 const watchFiles = () => {
